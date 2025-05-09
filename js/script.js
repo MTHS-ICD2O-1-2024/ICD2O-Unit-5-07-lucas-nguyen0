1bd47cb87627fb19f173166a882d04c5ab8a765a
default <lucas.nguyen@mths.ca>
2025-05-09 17:23:37 +0000

    coed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Lucas Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lucas Nguyen
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+
+function enterClicked() {
+  let inputNumber = parseFloat(document.getElementById('input-number-one').value)
+  let answer = 0
+  for (let counter = 0; counter <= inputNumber; counter++) {
+    answer = answer + counter
+  } 
+  document.getElementById("answer").innerHTML =
+    'The answer is: ' + answer + '.'
+}
